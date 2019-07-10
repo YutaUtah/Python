ace4a4bd7cb5c763c7e1d48a6e3d77f69cf94c31
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2019-07-10 17:59:46 -0500

    Lasso Ridge

---
+++ lasso:ridge.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ridge regression / Lasso regression 
+
+# In[42]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.random import *
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
+from sklearn.datasets import load_boston
+
+# sample data gotten from sklearn.dataset
+dataset = load_boston()
+
+
+# In[43]:
+
+
+# sample data
+data_x = pd.DataFrame(dataset.data,columns=dataset.feature_names)
+
+#target data
+data_y = pd.DataFrame(dataset.target,columns=['target'])
+
+
+# In[44]:
+
+
+#compact the data set 
+size = len(data_x) - 10
+for i in range(size):
+    data_x = data_x.drop(i)
+    data_y = data_y.drop(i)
+
+data_y = data_y.as_matrix().ravel()
+
+
+# In[45]:
+
+
+lr = LinearRegression()
+lasso = Lasso()
+ridge = Ridge()
+
+
+# In[46]:
+
+
+#create regression linear / lasso / ridge
+lr.fit(data_x, data_y)
+lasso.fit(data_x, data_y)
+ridge.fit(data_x, data_y)
+
+
+# In[47]:
+
+
+plt.plot(lr.predict(data_x), linestyle="solid", 
+         color="black",label="lr")
+plt.plot(lasso.predict(data_x), linestyle="solid", 
+         color="red", label="lasso")
+plt.plot(ridge.predict(data_x), linestyle="solid", 
+         color="blue", label='ridge')
+plt.title("Linear, Lasso, Ridge")
+plt.legend(loc="lower right")
+plt.show()
+
+
+# In[51]:
+
+
+print('coefficients of lasso and aridge')
+
+reg_coef=[]
+reg_coef.append([lasso.coef_])
+reg_coef.append([ridge.coef_])
+print(reg_coef)
+
